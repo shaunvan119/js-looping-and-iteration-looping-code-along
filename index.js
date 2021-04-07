3e2a274c4e2d6a6ee6cd278dac0a628b0e40472d
shaunvan119 <shaunvan@live.com.au>
2021-04-07 20:38:54 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+      console.log(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards
+  }
+
+  writeCards([ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise');
+
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+
+}
+countDown(10)
